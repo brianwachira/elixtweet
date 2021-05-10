9af692ad9c0551cbe7bd3518a39e57a4b0782a7d
brianwachira <brianwachira7@gmail.com>
2021-05-10 22:08:02 +0300

    make initial commit

---
+++ lib/pluralsight_tweet/file_reader.ex
@@ -0,0 +1,8 @@
+defmodule PluralsightTweet.FileReader do
+  def get_strings_to_tweet(path) do
+      File.read!(path)
+      |> String.split("\n")
+      |> Enum.map(&String.trim/1)
+      |> Enum.filter(fn str -> String.length(str) <= 140 end)
+  end
+end
